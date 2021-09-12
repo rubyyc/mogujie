b870ae6427e4827372f2439f684b74ba7b30db33
YuanChang <759337828@qq.com>
2021-09-12 15:02:36 +0800

    mock方式优化

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,26 +1,5 @@
 import Mock from 'mockjs'
-
-Mock.mock(
-  '/api/users',
-  'get',
-  () => {
-    return Mock.mock({
-      status: 200,
-      msg: '请求成功',
-      // 产生3条数据
-      'data|3': [
-        {
-          // 随机产生名称
-          // 1.函数写法
-          // name: Mock.Random.cname(),
-          // 2.@canme写法
-          name: '@cname',
-          // 随机产生20-50的年龄
-          age: '@integer(20,50)'
-        }
-      ]
-    })
-  }
-)
+import mall from './mail'
+Mock.mock('/api/users', 'get', mall.getUsers)
 
 export default Mock
---
+++ src/mock/mail.js
@@ -0,0 +1,21 @@
+import Mock from 'mockjs'
+export default {
+  getUsers: () => {
+    return Mock.mock({
+      status: 200,
+      msg: '请求成功',
+      // 产生3条数据
+      'data|3': [
+        {
+          // 随机产生名称
+          // 1.函数写法
+          // name: Mock.Random.cname(),
+          // 2.@canme写法
+          name: '@cname',
+          // 随机产生20-50的年龄
+          age: '@integer(20,50)'
+        }
+      ]
+    })
+  }
+}
