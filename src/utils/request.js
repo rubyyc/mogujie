392018c558ea1dd2fb69251aedfe8ea3591c1281
YuanChang <759337828@qq.com>
2021-09-12 14:30:15 +0800

    axios的安装,封装,拦截器的配置,发起请求

---
+++ src/utils/request.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const service = axios.create({})
+
+service.interceptors.request.use(
+  config => {
+    return config
+  }
+)
+
+service.interceptors.response.use(
+  response => {
+    const data = { response }
+    return data
+  }
+)
+
+export default service
