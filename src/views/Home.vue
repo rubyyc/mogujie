eb611f90df047977d18a7cfe4384ae891fa0755b
YuanChang <759337828@qq.com>
2021-09-15 11:51:48 +0800

    css实现轮播图效果

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,12 @@
     <hamburger-button theme="multi-color" size="34" :fill="['#9c9393' ,'#326fc2' ,'#FFF' ,'#43CCF8']" :strokeWidth="2" />
     <star theme="multi-color" size="24" :fill="['#c79a41' ,'#f5a623' ,'#FFF' ,'#43CCF8']" />
   </div>
+  <div class="box">
+    <div class="box__item box__item--bg1"></div>
+    <div class="box__item box__item--bg2"></div>
+    <div class="box__item box__item--bg3"></div>
+    <div class="box__item box__item--bg4"></div>
+  </div>
 </template>
 
 <script>
@@ -23,5 +29,30 @@ export default {
 <style lang="scss" scoped>
 .btn {
 	@include dimensions(75px, 25px);
+}
+.box{
+  scroll-snap-type: x mandatory;
+  overflow: auto;
+  white-space: nowrap;
+  &__item{
+    display: inline-block;
+    height: 100px;
+    width: 100%;
+    background-color: pink;
+    scroll-snap-align: center;
+    scroll-snap-stop: always;
+    &--bg1{
+      background-color: pink;
+    }
+    &--bg2{
+      background-color: rgb(173, 111, 41);
+    }
+    &--bg3{
+      background-color: rgb(20, 19, 19);
+    }
+    &--bg4{
+      background-color: rgb(209, 192, 255);
+    }
+  }
 }
 </style>
